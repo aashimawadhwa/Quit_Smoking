e0ef8b69a4c832a244a7d49d07ebeb86a0027a23
aashimawadhwa <ashimawadhwa1208@gmail.com>
2021-09-26 01:44:39 +0530

    signup page

---
+++ src/signup.js
@@ -0,0 +1,15 @@
+const signup  = () => {
+    return(
+        
+        <>
+        <section className="signup">
+            <div className = "container mt-5">
+                <div className= "signup-content">
+
+                </div>
+            </div>
+        </section>
+        </>
+    )
+}
+ export default signup
