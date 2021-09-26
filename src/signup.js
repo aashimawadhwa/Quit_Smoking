df0794be5d01cd0e873ac2b15486c6d8960a0b7a
aashimawadhwa <ashimawadhwa1208@gmail.com>
2021-09-26 22:11:20 +0530

    signup added

--- src/signup.js
+++ src/signup.js
@@ -1,3 +1,5 @@
+import react from "react"
+
 const signup  = () => {
     return(
         
@@ -5,7 +7,44 @@ const signup  = () => {
         <section className="signup">
             <div className = "container mt-5">
                 <div className= "signup-content">
+                    <div className="sign-up form">
+                        <h2 className = "form-tittle">Sign Up</h2>
+                        <form className = "register-form" id="register-form">
+                            <div className ="form-group">
+                                <label htmlFor ="Name">
+                                    <i className ="zmdi zmdi-account material-icon-name"></i>
 
+                                </label>
+                                <input type="text" name="name" id="name" autoComplete="off" placeholder ="your name"></input>
+                            </div>
+
+                            <div className ="form-group">
+                                <label htmlFor ="Email Address">
+                                    <i className ="zmdi zmdi-email material-icon-name"></i>
+
+                                </label>
+                                <input type="text" name="Email Address" id="Email Address" autoComplete="off" placeholder ="Email Address"></input>
+                            </div>
+
+                            <div className ="form-group">
+                                <label htmlFor ="password">
+                                    <i className ="zmdi zmdi-lock material-icon-name"></i>
+
+                                </label>
+                                <input type="text" name="password" id="name" autoComplete="off" placeholder ="Password"></input>
+                            </div>
+
+                            <div className ="form-group">
+                                <label htmlFor ="Confirm Password">
+                                    <i className ="zmdi zmdi-lock material-icon-name"></i>
+
+                                </label>
+                                <input type="text" name="Confirm Password" id="name" autoComplete="off" placeholder ="Confirm Password"></input>
+                            </div>
+
+                            
+                        </form>
+                    </div>
                 </div>
             </div>
         </section>
