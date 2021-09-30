ebbf5ca320d5cb1533998d21b5f7daf1640445b6
aashimawadhwa <ashimawadhwa1208@gmail.com>
2021-10-01 02:47:45 +0530

    homepage

--- src/homepage.js
+++ src/homepage.js
@@ -3,6 +3,7 @@ import react from "react"
 import './homepage.scss';
 import logo from './logo.png'
 import "./footer.css";
+import homepage1 from './homepage1.png'
 
 const homepage  = () => {
     return(
@@ -24,6 +25,57 @@ const homepage  = () => {
          <a class="btn" href="#">streak</a>
          <a class="btn" href="#">About</a>
         </div>
+
+        <div class="container1">
+          <div class="row">
+                <div class="col">
+                    <p>IT RUINS YOUR LIFE<br></br>HER LIFE <br></br>AND THIER LIFE</p>
+                    <div className ="cbtn">
+
+                    <a href="#"><span></span>QUIT NOW</a>
+        
+                    </div>
+                </div>
+          </div>
+         </div>
+         <div class="container2">
+          <div class="row">
+                <div class="col">
+               
+                            <figure>
+                                <img src ={homepage1} alt="healthy"/>
+                            </figure>
+
+                       
+        
+                </div>
+          </div>
+         </div>
+         <div class="container3">
+          <div class="row">
+                <div class="col">
+               
+                <p>IT RUINS YOUR LIFE<br></br>HER LIFE <br></br>AND THIER LIFE</p>
+  
+
+                       
+        
+                </div>
+          </div>
+         </div>
+         <div class="container4">
+          <div class="row">
+                <div class="col">
+               
+                <p>IT RUINS YOUR LIFE<br></br>HER LIFE <br></br>AND THIER LIFE</p>
+   
+
+                       
+        
+                </div>
+          </div>
+         </div>
+
 
         
         
