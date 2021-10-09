c79ed5737800cea36dfbba337194d39859f0358b
aashimawadhwa <ashimawadhwa1208@gmail.com>
2021-10-10 03:42:00 +0530

    added timer

--- src/App.js
+++ src/App.js
@@ -9,11 +9,19 @@ import day1 from './day1';
 import './day1';
 // import './login';
 // import login from './login';
+import CountDownTimer from './countDownTimer';
+import './countDownTimer';
 
 
 function App() {
 
-  return day1();
+  const hoursMinSecs = {hours:0, minutes: 15, seconds: 0}
+  
+  return (
+    <div className="App">
+        <CountDownTimer hoursMinSecs={hoursMinSecs}/>
+    </div>
+  );
 }
 
 export default App;
--- src/day1.js
+++ src/day1.js
@@ -1,26 +1,60 @@
 import { logDOM } from "@testing-library/react";
 import react from "react";
-import Card from "react-bootstrap/Card";
-import ListGroup from "react-bootstrap/ListGroup";
-
-
-
+import './day1.css';
+import "./footer.css";
+import logo from './logo.png'
 
 
 const day1  = () => {
     return(
         
         <>
-        
-        <Card style={{ width: '200rem' }}>
-  <Card.Header>Featured</Card.Header>
-  <ListGroup variant="flush">
-    <ListGroup.Item>Cras justo odio</ListGroup.Item>
-    <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-    <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-  </ListGroup>
-</Card>
-       
+         <div className="logo">
+                            <figure>
+                                <img src ={logo} alt="healthy" height="150px" width="180px"/>
+                            </figure>
+                            <div className="head">
+           
+           <p className="head-content"> DAY 1 <br></br> <br></br></p>
+              
+           </div>
+
+        </div>
+        <div className= "table">
+      <table border="10" width="1000" height="500">
+        <tr>
+          <td> Meditate</td>
+          <td> row 1 col 2</td>
+         </tr>
+         <tr>
+          <td> Breathing Exercise </td>
+          <td> row 1 col 2</td>
+         </tr>
+         <tr>
+          <td> Running</td>
+          <td> row 1 col 2</td>
+         </tr>
+         <tr>
+          <td>Laugh</td>
+          <td> row 1 col 2</td>
+         </tr>
+         <tr>
+          <td>Talk your heart out  to someone you trust</td>
+          <td> row 1 col 2</td>
+         </tr>
+         
+      </table>
+      </div>
+
+
+
+      <footer>
+        <div className="footer">
+    <p>Made with ♥️ by Aashima Wadhwa</p>
+  </div>
+
+        </footer>
+    
        
         </>
 
